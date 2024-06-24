33bb8f28c0a28830b6708d3821f598fd5734f13c
jayathu sanka <shjayathusanka@gmail.com>
2024-06-24 19:17:25 +0530

    all done

---
+++ Labsheet2/Q3.scala
@@ -0,0 +1,10 @@
+object question03{
+    def main(args: Array[String]): Unit ={
+    
+    
+    val salary= 40*250+30*85;
+        val home_salary= salary*0.88;
+        println(home_salary);
+    
+    }
+}
